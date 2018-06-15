94fd394b9dfd81154579024f3aeb422d95ba9f63
qMaDMaNp <medifman@gmail.com>
2018-06-15 16:36:20 +0300

    edbut v1

--- public/javascripts/editButton.js
+++ public/javascripts/editButton.js
@@ -1,17 +1,65 @@
-var editButtons = document.getElementsByName("edit-button");
-var ourUL = editButtons[1].parentNode.parentNode;
-
-for(keys in ourUL.children) {
-    if(ourUL.children[keys].classList.contains('title')){
-        console.log(ourUL.children[1].textContent)
-    } else {
-        console.log(22323421);
-    }
-}
+let editButtons = document.getElementsByName("edit-button");
+let pageBody = document.getElementsByTagName("body")[0];
 
 
 editButtons.forEach((elem) => {
-    elem.addEventListener('click', () => {
+    elem.addEventListener('click',  () => {
+        let elemListLine = elem.parentNode.parentNode,
+            elemTitleItem = null,
+            elemTitle = null,
+            elemStartItem = null,
+            elemStart = null,
+            elementEndItem = null,
+            elemEnd = null;
+
+        for(let keys in elemListLine.children) {
+            if(elemListLine.children[keys].classList.contains('title')) {
+                elemTitleItem = elemListLine.children[keys];
+                elemTitle = elemListLine.children[keys].textContent;
+            }
+            if(elemListLine.children[keys].classList.contains('start-time')) {
+                elemStartItem = elemListLine.children[keys];
+                elemStart = elemListLine.children[keys].textContent;
+            }
+            if(elemListLine.children[keys].classList.contains('end-time')) {
+                elementEndItem = elemListLine.children[keys];
+                elemEnd = elemListLine.children[keys].textContent;
+                break;
+            }
+        }
+        console.log(elemTitle, elemStart, elemEnd);
+        //CREATE INPUTS for editing with a values elemTitle, elemStart, elemEnd
+        let titleInput = document.createElement('input');
+            titleInput.type = 'text';
+            titleInput.value = elemTitle;
+
+        let startInput = document.createElement('input');
+            startInput.type = 'text';
+            startInput.value = elemStart;
+
+        let endInput = document.createElement('input');
+            endInput.type = 'text';
+            endInput.value = elemEnd;
+
+        //Delete elements
+        elemTitleItem.removeChild(elemTitleItem.getElementsByTagName('p')[0]);
+        elemStartItem.removeChild(elemStartItem.getElementsByTagName('p')[0]);
+        elementEndItem.removeChild(elementEndItem.getElementsByTagName('p')[0]);
+
+
+        //Append inputs
+        elemTitleItem.appendChild(titleInput);
+        elemStartItem.appendChild(startInput);
+        elementEndItem.appendChild(endInput);
+
+        //Add new elements
+
+
+        //Send server request with data;
+
 
     });
 });
+
+
+
